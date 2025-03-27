4c5dc6d41de68138d7c8f38b78ce16214c45ac49
PpAaMm19945 <antmwes104.1@gmail.com>
2025-03-27 08:01:00 +0300

    Missing Files causing error

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Home-Line Academy!</h1>
+      <p>Welcome to our website.</p>
+    </div>
+  );
+}
+
+export default App;
