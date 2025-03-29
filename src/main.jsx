bf65e012fa67143e14aeaad26081d399ff81e9fa
PpAaMm19945 <antmwes104.1@gmail.com>
2025-03-29 15:10:08 +0300

    Cleaning Up

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Curriculum from './pages/Curriculum.jsx'
+import Testimonials from './pages/Testimonials.jsx'
+import Contact from './pages/Contact.jsx'
+import Demo from './pages/Demo.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/curriculum" element={<Curriculum />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
